4d8f6dc1d52f7c2aaa7cf9005438125bac81ef5c
brycefish <bryce.k.fish@gmail.com>
2023-09-21 19:17:26 -0600

    Add simple console menu (ch03)

--- bf_baseball_mgmt_gui.py
+++ bf_baseball_mgmt_gui.py
@@ -2,21 +2,35 @@
 # Bryce Fish
 # Project 1
 # Server-Side Programming
-# Sept 16: parts 1-2
 
 # display a welcome message
 print("================================================================")
 print("\t\tBaseball Team Manager\n")
 print("This program calculates the batting average for a player based \n on the player\'s official number of at bats and hits.")
-print("================================================================")
-# get user input
-player_name_input = (input("Player's Name: "))
-num_bats_input = int(input("Official number of at bats: "))
-num_hits_input = int(input("Number of hits: "))
-
-#calc avg and round
-batting_avg = num_hits_input / num_bats_input
-batting_avg = round(batting_avg, 3)
-
 print()
-print(f"{player_name_input}'s batting average is {batting_avg}")
+print("1 – Calculate batting average")
+print("2 - Exit program")
+print("================================================================")
+
+
+exitChar = True
+while exitChar == True:
+    print()
+    option = input("Menu option: ")
+
+    if option == '1':
+        print("-- Calculate batting average --")
+        num_bats_input = int(input("Official number of at bats: "))
+        num_hits_input = int(input("Number of hits: "))
+
+        #calc avg and round
+        batting_avg = num_hits_input / num_bats_input
+        batting_avg = round(batting_avg, 3)
+        print(f"Batting average: {batting_avg}")
+
+    elif option == '2':
+        print("Bye!")
+        exitChar = False
+
+    else:
+        print("Not a valid option. Please try again")
